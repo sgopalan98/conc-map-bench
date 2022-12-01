27eacc5a96b1da3695c21d77e56044bcca5270a0
Santhanagopalan <sgopalan58@gmail.com>
2022-12-01 13:52:10 -0600

    Fixed server thread closing bug

--- src/adapters/mod.rs
+++ src/adapters/mod.rs
@@ -1,17 +1,18 @@
-pub use self::{
-    btreemap::RwLockBTreeMapTable, chashmap::CHashMapTable, contrie::ContrieTable,
-    crossbeam_skiplist::CrossbeamSkipMapTable, dashmap::DashMapTable, evmap::EvmapTable,
-    flurry::FlurryTable, std::RwLockStdHashMapTable,
-};
+// pub use self::{
+//     btreemap::RwLockBTreeMapTable, chashmap::CHashMapTable, contrie::ContrieTable,
+//     crossbeam_skiplist::CrossbeamSkipMapTable, dashmap::DashMapTable, evmap::EvmapTable,
+//     flurry::FlurryTable, std::RwLockStdHashMapTable,
+// };
+pub use self::dashmap::DashMapTable;
 
-mod btreemap;
-mod chashmap;
-mod contrie;
-mod crossbeam_skiplist;
+// mod btreemap;
+// mod chashmap;
+// mod contrie;
+// mod crossbeam_skiplist;
 mod dashmap;
-mod evmap;
-mod flurry;
-mod std;
-pub mod striped;
+// mod evmap;
+// mod flurry;
+// mod std;
+// pub mod striped;
 
 type Value = u32;
